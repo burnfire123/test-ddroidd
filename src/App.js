ff2e1733ed9535970b0cdf8db0f1fc8193775333
burnfire <stefanescupetru@yahoo.com>
2021-12-22 04:05:34 +0200

    App main background.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import Switch from "react-router-dom/es/Switch";
 import Route from "react-router-dom/es/Route";
+import {PresentationPage} from "./components/presentation-page/PresentationPage";
 
 /*No hard feelings, but React routing is not cool at all. 2 big libraries with almost the same name, which have changes
 * in their function names, no documentation for the lastest version*/
@@ -13,9 +14,11 @@ function App() {
     <BrowserRouter>
         <div>
             <NavBar/>
-            <Switch>
-                <Route path={"/ceva"}><h1>Ceva</h1></Route>
-            </Switch>
+            <div id={"appBackground"}>
+                <Switch>
+                    <Route exact path={"/"}><PresentationPage/></Route>
+                </Switch>
+            </div>
         </div>
     </BrowserRouter>
   );
---
+++ src/components/presentation-page/PresentationPage.js
@@ -0,0 +1,5 @@
+export const PresentationPage = () => {
+  return (<div>
+    This works!
+  </div>);
+}
