97574e52495f2c593a28d691c907f6b2c41a6a3a
burnfire <stefanescupetru@yahoo.com>
2021-12-23 02:49:04 +0200

    404 'page' added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Switch from "react-router-dom/es/Switch";
 import Route from "react-router-dom/es/Route";
 import {PresentationPage} from "./components/presentation-page/PresentationPage";
 import {SignupForm} from "./components/signup-form/SignupForm";
+import {FinishPage} from "./components/finish-page/FinishPage";
 
 /*No hard feelings, but React routing is not cool at all. 2 big libraries with almost the same name, which have changes
 * in their function names, no documentation for the lastest version*/
@@ -20,6 +21,8 @@ function App() {
                 <Switch>
                     <Route exact path={"/"}><PresentationPage/></Route>
                     <Route path={"/join"}><SignupForm/></Route>
+                    <Route path={"/finish"}><FinishPage/></Route>
+                    <Route path={"*"}><h1 style={{color: "red"}}>404! This page isn't found!</h1> </Route>
                 </Switch>
             </main>
         </div>
---
+++ src/components/finish-page/FinishPage.js
@@ -0,0 +1,5 @@
+export const FinishPage = () => {
+  return (<div>
+      
+  </div>);
+}
